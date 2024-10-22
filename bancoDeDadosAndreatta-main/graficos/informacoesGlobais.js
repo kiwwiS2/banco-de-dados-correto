cd8c24769a484bb2ef3db2c234a8109ca8e48ead
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-10-22 12:21:48 -0300

    Update informacoesGlobais.js

--- bancoDeDadosAndreatta-main/graficos/informacoesGlobais.js
+++ bancoDeDadosAndreatta-main/graficos/informacoesGlobais.js
@@ -6,12 +6,18 @@ async function visualizarInformacoesGlobais() {
     const res= await fetch(url)
     const dados = await res.json
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_de_pessoas_mundo / 1e9)
+    const pessoasNoMundo = (dados.total_de_pessoas_mundo / 1e9) paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_de_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão em alguma rede social e passam em média <span>${dados.tempo_medio}</span>horas conectadas.`
+    const horas = parseInt(dados.empo_medio)
+    const minutos = Match.round((dados.tempo_MEDIO - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasnoMundo ) * 100).toFixed(2)
+
+
+
+}
+
     console.log(dados);
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_de_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão em alguma rede social e passam em média <span>${dados.tempo_medio}</span>horas conectadas.`
-}
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
 visualizarInformacoesGlobais();
-
